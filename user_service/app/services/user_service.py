4259d200676c337eb38ad7236192ef2fd3f5fb13
MorisFromRTU <arme-kara@mail.ru>
2025-03-13 14:02:28 +0300

    refactor: user-service.py PEP8 update

--- user_service/app/services/user_service.py
+++ user_service/app/services/user_service.py
@@ -7,15 +7,15 @@ from app.schemas import UserCreate, UserLogin
 from fastapi import HTTPException, status
 import bcrypt
 import httpx
-import os
+
 
 class UserService:
     def __init__(self, db_session: AsyncSession):
         self.db = db_session
         self.repository = UserRepository(db_session)
         self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-        self.auth_service_url: str = "http://auth-service:8000" 
-    
+        self.auth_service_url: str = "http://auth-service:8000"
+
     async def get_all_users(self) -> list[User]:
         """Бизнес-логика получения всех пользователей"""
         return await self.repository.get_all_users()
@@ -24,43 +24,66 @@ class UserService:
         """Бизнес-логика получения пользователя по ID"""
         return await self.repository.get_user_by_id(user_id)
 
-    async def verify_password(self, plain_password: str, hashed_password: str) -> bool:
+    async def verify_password(
+            self,
+            plain_password: str,
+            hashed_password: str) -> bool:
         """Проверка корректности пароля"""
         return self.pwd_context.verify(plain_password, hashed_password)
 
-    async def authenticate_user(self, db: AsyncSession, username: str, password: str) -> User:
+    async def authenticate_user(
+            self,
+            db: AsyncSession,
+            username: str,
+            password: str) -> User:
         user = await self.repository.get_user_by_username(username=username)
         if not user:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="User not found"
             )
-        
+
         if not await self.verify_password(password, user.password):
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="Incorrect password"
             )
         return user
-    
-    async def create_user(self, user_data: UserCreate):
-        """Бизнес логика регистрации пользователя"""
-        if await self.repository.get_user_by_username(username=user_data.username):
-            raise ValueError("Пользователь с таким именем ником уже сущетсвуте")
-        
-        if await self.repository.get_user_by_email(email=user_data.email):
-            raise ValueError("Пользователь с такой почтой уже сущетсвуте")
 
-        hashed_password = bcrypt.hashpw(user_data.password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
-        
+    async def create_user(self, user_data: UserCreate):
+        """Бизнес-логика регистрации пользователя"""
+        if await self.repository.get_user_by_username(
+            username=user_data.username
+        ):
+            raise ValueError(
+                "Пользователь с таким именем ником уже сущетсвует"
+            )
+
+        if await self.repository.get_user_by_email(
+            email=user_data.email
+        ):
+            raise ValueError(
+                "Пользователь с такой почтой уже сущетсвует"
+            )
+
+        hashed_password = bcrypt.hashpw(user_data.password.encode("utf-8"),
+                                        bcrypt.gensalt()).decode("utf-8")
+
         if not hashed_password:
             raise ValueError('Ошибка при создании пользователя')
-        
-        return await self.repository.create_user(user_data=user_data, hashed_password=hashed_password)
-    
+
+        return await self.repository.create_user(
+            user_data=user_data,
+            hashed_password=hashed_password
+            )
+
     async def user_login(self, user_data: UserLogin, db: AsyncSession):
         """Бизнес-логика логина пользователя"""
-        user = await self.authenticate_user(username=user_data.username, password=user_data.password, db=db)
+        user = await self.authenticate_user(
+            username=user_data.username,
+            password=user_data.password,
+            db=db
+        )
         if not user:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
@@ -69,10 +92,19 @@ class UserService:
         user_data: dict = {"user_id": user.id}
         async with httpx.AsyncClient() as client:
             try:
-                token = await client.post(f"{self.auth_service_url}/auth/token", json=user_data)
+                token = await client.post(
+                    f"{self.auth_service_url}/auth/token",
+                    json=user_data
+                )
                 token.raise_for_status()
                 return token.json()
             except httpx.RequestError as e:
-                raise HTTPException(status_code=500, detail=f"Ошибка при обращении к user-service: {e}")
+                raise HTTPException(
+                    status_code=500,
+                    detail=f"Ошибка при обращении к user-service: {e}"
+                )
             except httpx.HTTPStatusError as e:
-                raise HTTPException(status_code=e.response.status_code, detail=e.response.text)
+                raise HTTPException(
+                    status_code=e.response.status_code,
+                    detail=e.response.text
+                )
